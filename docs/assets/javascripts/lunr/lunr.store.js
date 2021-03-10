9a0d9a7848349887fd7475f2a42044e3e7bb0763
mcint170 <mcint170@msu.edu>
2021-03-10 14:52:48 -0500

    Realized I need to use jekyll build to get sitemap working correctly

--- docs/assets/javascripts/lunr/lunr.store.js
+++ docs/assets/javascripts/lunr/lunr.store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "Inaugural Post",
         "excerpt":"This marks the first offical post on my personal website. If you are seeing thisnow, welcome 😃 I hope you find the content here somewhat enjoyable or atleast a tad bit interesting. If you are seeing this relatively recently to posting, please come back later and I hope to be...","categories": ["Annoucement"],
         "tags": ["Announcement","Special"],
-        "url": "http://localhost:4000/annoucement/2021/03/09/Inaugural-Post.html"
+        "url": "https://mcint170.github.io//annoucement/2021/03/09/Inaugural-Post.html"
       },{
         "title": "Cookies",
         "excerpt":"","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/projects/2021-03-09-chocolate-chip-cookies"
+        "url": "https://mcint170.github.io//projects/2021-03-09-chocolate-chip-cookies"
       },{
         "title": "Visualizing Lansing Traffic Stop Data",
         "excerpt":"&lt;p style=\"text-align: right;\"&gt; Brandon McIntyre &lt;/p&gt; &lt;p style=\"text-align: right;\"&gt; Analysis of Lansing Traffic Stops CMSE 402 SS20 &lt;/p&gt; Final Writeup Data Used Traffic Stop Data of Lansing Police Department (02/01/2016 - 02/13/2019):http://data-lansing.opendata.arcgis.com/datasets/32b48df10c674a7aace77071046ba272_0?selectedAttribute=Date Census Data/ ACS Data:https://data.census.gov/cedsci/table?q=Lansing%20MI&amp;g=1600000US2646000&amp;hidePreview=false&amp;tid=ACSDP5Y2018.DP05&amp;vintage=2010&amp;layer=VT_2018_160_00_PY_D1&amp;cid=DP05_0001E Note Looking at Lansing Police Department MATS Data Analysis for Year 17 (https://lansingmi.gov/DocumentCenter/View/6979/MATS-Months193-204-May-2018) it...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/projects/Final_poster_edit"
+        "url": "https://mcint170.github.io//projects/Final_poster_edit"
       },{
         "title": "Oatmeal Cookies",
         "excerpt":"Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts. Ingredients 1 cup butter, softened 1 cup white sugar 1 cup packed brown sugar 2 eggs 1 teaspoon vanilla extract 2 cups all-purpose flour 1...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/projects/oatmeal-cookies"
+        "url": "https://mcint170.github.io//projects/oatmeal-cookies"
       },{
         "title": "Peanut Butter Cookies",
         "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/projects/peanut-butter-cookies"
+        "url": "https://mcint170.github.io//projects/peanut-butter-cookies"
       }]
